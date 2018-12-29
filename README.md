9ece1f9d09ca939ce5b6ca2c2415f0364b3b9463
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2018-12-29 01:27:36 -0500

    New functionality: add column, enter grades

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ sqlite3
 
 nodejs
 
-Once nodejs is installed, add dependencies: `npm install commander inquirer better-sqlite3`
+Once nodejs is installed, add dependencies: `npm install commander inquirer better-sqlite3 fuse.js`
 
 ## Demo
 `./b2d2 --help` (or just `./b2d2 -h` for the lazy)
--- index.js
+++ index.js
@@ -63,9 +63,83 @@ const showGradebook = () => {
 
 
 
+const addGradeColumn = (colname) => {
+  const sql_stmt = 'sqlite3 gbook.db "ALTER TABLE gbook ADD COLUMN ' + colname + ' FLOAT"';
+  const { exec } = require('child_process');
+  exec(sql_stmt, (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+    //console.log(sql_stmt);
+  });
+}
+
+
+
+const addGrade = (colname, student, score) => {
+  const sid = getStudentIdFromSearch(student);
+  const sql_stmt = 'sqlite3 gbook.db "UPDATE gbook SET ' + colname + ' = ' + score + ' WHERE sid = ' + sid + '"';
+  const { exec } = require('child_process');
+  exec(sql_stmt, (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+    //console.log(sql_stmt);
+  });
+}
+
+
+
+const getStudentIdFromSearch = (searchString) => {
+  const db = require('better-sqlite3')('gbook.db');
+  const sql_get_full_list = db.prepare('SELECT last,first,middle,pref,sid FROM gbook');
+  const full_list = sql_get_full_list.all();
+
+  const full_list_squished = [];
+  for(var i = 0; i < full_list.length; i++) {
+    const squished_name = full_list[i].first + full_list[i].pref + full_list[i].last;
+    full_list_squished.push({name: squished_name, sid: full_list[i].sid});
+  }
+
+  var options = {
+    caseSensitive: false,
+    shouldSort: true,
+    tokenize: true,
+    matchAllTokens: true,
+    threshold: 0.4, // 0.0 will only return exact matches, 1.0 will return anything
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: ["name"]
+  };
+
+  const Fuse = require('fuse.js');
+  var fuse = new Fuse(full_list_squished, options);
+  var search_result = fuse.search(searchString);
+
+  if(search_result.length == 0) {
+    console.log("no students named " + searchString + " found");
+    return;
+  }
+  else if(search_result.length != 1) {
+    console.log("Possible matches:");
+    console.log(search_result);
+    return;
+  }
+  else if(search_result.length == 1) {
+    //console.log(search_result[0].sid);
+    return search_result[0].sid;
+  }
+}
+
+
+
 module.exports = {
   initializeGradebook,
   addStudent,
   importRoster,
-  showGradebook
+  showGradebook,
+  addGradeColumn,
+  addGrade
 }
